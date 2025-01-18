a9db271ab4855e494365d2b98d8ad33dc65a60ca
OTDF Studios Official <168537533+OTDFStudiosOFFICIAL@users.noreply.github.com>
2025-01-18 01:34:18 +0000

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ CHANNEL_ID = "YOUR_CHANNEL_ID"
 THUMBNAILS_DIR = "data/thumbnails"
 CSV_FILE = "data/video_data.csv"
 
-# Date range
+# Date range - You can basically change this yourself
 START_DATE = "2023-12-15T00:00:00Z"
 END_DATE = "2025-01-17T00:00:00Z"
