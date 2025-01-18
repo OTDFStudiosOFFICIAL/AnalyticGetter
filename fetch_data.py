e89b65b19ca047734e01e2d3f5395250dd9dd397
OTDF Studios Official <168537533+OTDFStudiosOFFICIAL@users.noreply.github.com>
2025-01-18 01:33:43 +0000

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -1,47 +1,65 @@
+# fetch_data.py
+
 from googleapiclient.discovery import build
 from config import API_KEY
 
 def youtube_api_client(api_key):
     return build('youtube', 'v3', developerKey=api_key)
 
-def fetch_video_data(api_key, channel_id, start_date, end_date):
+def fetch_video_data(api_key, channel_id):
     youtube = youtube_api_client(api_key)
     video_data = []
-    
-    # YouTube API request for channel's videos
-    request = youtube.search().list(
-        part="snippet",
-        channelId=channel_id,
-        publishedAfter=start_date,
-        publishedBefore=end_date,
-        maxResults=50  # Adjust based on your needs (max 50 per request)
+
+    # Fetch the uploads playlist ID from the channel
+    channels_request = youtube.channels().list(
+        part="contentDetails",
+        id=channel_id
     )
-    response = request.execute()
-    
-    for item in response['items']:
-        video_id = item['id']['videoId']
-        video_title = item['snippet']['title']
-        video_thumbnail_url = item['snippet']['thumbnails']['high']['url']
-        
-        # Get video statistics (views, likes, comments, etc.)
-        stats_request = youtube.videos().list(
-            part="statistics",
-            id=video_id
+    channels_response = channels_request.execute()
+
+    # Get the playlistId of the "uploads" playlist
+    uploads_playlist_id = channels_response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+
+    # Fetch all videos from the uploads playlist
+    next_page_token = None
+    while True:
+        request = youtube.playlistItems().list(
+            part="snippet",
+            playlistId=uploads_playlist_id,
+            maxResults=50,  # Max results per request
+            pageToken=next_page_token
         )
-        stats_response = stats_request.execute()
-        
-        stats = stats_response['items'][0]['statistics']
-        views = stats.get('viewCount', 0)
-        likes = stats.get('likeCount', 0)
-        comments = stats.get('commentCount', 0)
-        
-        video_data.append({
-            'Video ID': video_id,
-            'Title': video_title,
-            'Thumbnail URL': video_thumbnail_url,
-            'Views': views,
-            'Likes': likes,
-            'Comments': comments
-        })
+        response = request.execute()
+
+        for item in response['items']:
+            video_id = item['snippet']['resourceId']['videoId']
+            video_title = item['snippet']['title']
+            video_thumbnail_url = item['snippet']['thumbnails']['high']['url']
+
+            # Get video statistics (views, likes, comments, etc.)
+            stats_request = youtube.videos().list(
+                part="statistics",
+                id=video_id
+            )
+            stats_response = stats_request.execute()
+            
+            stats = stats_response['items'][0]['statistics']
+            views = stats.get('viewCount', 0)
+            likes = stats.get('likeCount', 0)
+            comments = stats.get('commentCount', 0)
+            
+            video_data.append({
+                'Video ID': video_id,
+                'Title': video_title,
+                'Thumbnail URL': video_thumbnail_url,
+                'Views': views,
+                'Likes': likes,
+                'Comments': comments
+            })
+
+        # If there are more pages, continue fetching them
+        next_page_token = response.get('nextPageToken')
+        if not next_page_token:
+            break
 
     return video_data
