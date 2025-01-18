d96b5ddca2c54686e815fea09f901a4730a74690
OTDF Studios Official <168537533+OTDFStudiosOFFICIAL@users.noreply.github.com>
2025-01-18 01:33:58 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@
 from fetch_data import fetch_video_data
 from download_thumbnails import download_thumbnails
 from export_csv import save_to_csv
-from config import API_KEY, CHANNEL_ID, START_DATE, END_DATE
+from config import API_KEY, CHANNEL_ID
 
 def main():
     # Fetch data from YouTube API
-    video_data = fetch_video_data(API_KEY, CHANNEL_ID, START_DATE, END_DATE)
+    video_data = fetch_video_data(API_KEY, CHANNEL_ID)
     
     # Save video data to CSV
     save_to_csv(video_data)
