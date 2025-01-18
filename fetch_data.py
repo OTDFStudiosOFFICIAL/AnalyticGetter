6c0b2921284120bdb0038d1a2f0f477f4b01bea7
OTDF Studios Official <168537533+OTDFStudiosOFFICIAL@users.noreply.github.com>
2025-01-18 01:14:06 +0000

    Updated fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -1,29 +1,47 @@
 from googleapiclient.discovery import build
+from config import API_KEY
 
-def fetch_video_data(api_key, channel_id):
-    youtube = build('youtube', 'v3', developerKey=api_key)
+def youtube_api_client(api_key):
+    return build('youtube', 'v3', developerKey=api_key)
+
+def fetch_video_data(api_key, channel_id, start_date, end_date):
+    youtube = youtube_api_client(api_key)
     video_data = []
-    next_page_token = None
-
-    while True:
-        request = youtube.search().list(
-            part="snippet",
-            channelId=channel_id,
-            maxResults=50,
-            type="video",
-            pageToken=next_page_token
+    
+    # YouTube API request for channel's videos
+    request = youtube.search().list(
+        part="snippet",
+        channelId=channel_id,
+        publishedAfter=start_date,
+        publishedBefore=end_date,
+        maxResults=50  # Adjust based on your needs (max 50 per request)
+    )
+    response = request.execute()
+    
+    for item in response['items']:
+        video_id = item['id']['videoId']
+        video_title = item['snippet']['title']
+        video_thumbnail_url = item['snippet']['thumbnails']['high']['url']
+        
+        # Get video statistics (views, likes, comments, etc.)
+        stats_request = youtube.videos().list(
+            part="statistics",
+            id=video_id
         )
-        response = request.execute()
-
-        for item in response['items']:
-            video_data.append({
-                "Video ID": item['id']['videoId'],
-                "Title": item['snippet']['title'],
-                "Thumbnail URL": item['snippet']['thumbnails']['high']['url']
-            })
-
-        next_page_token = response.get('nextPageToken')
-        if not next_page_token:
-            break
+        stats_response = stats_request.execute()
+        
+        stats = stats_response['items'][0]['statistics']
+        views = stats.get('viewCount', 0)
+        likes = stats.get('likeCount', 0)
+        comments = stats.get('commentCount', 0)
+        
+        video_data.append({
+            'Video ID': video_id,
+            'Title': video_title,
+            'Thumbnail URL': video_thumbnail_url,
+            'Views': views,
+            'Likes': likes,
+            'Comments': comments
+        })
 
     return video_data
